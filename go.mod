7c13796097e6493731e894498ed2e13e882e62e5
Zukhra <kontakt.bostanova@yandex.ru>
2024-05-29 23:33:18 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Bostanova/route256_tgbot
+
+go 1.22.0
---
+++ internal/storage/storage.go
@@ -0,0 +1,8 @@
+package storage
+
+type Storage struct{}
+
+// NewStorage создает объект типа Storage и возвращает его
+func NewStorage() *Storage {
+	return &Storage{}
+}
